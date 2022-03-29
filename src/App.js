1c8663c571939facd9b210797456c454dcc4b2a6
William Ellison <waellison@gmail.com>
2022-03-28 23:21:51 -0400

    Removed test icon from App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Skills from './Skills';
-import CaptionedIcon from './CaptionedIcon';
 
 function App() {
   return (
     <div className="App">
       <Skills />
-      <CaptionedIcon name="Azure" icon="/icons/dep-azure.svg" />
     </div>
   );
 }
