df9209aee25d3e07c81bf9e1c55c0b25a32713e2
William Ellison <97247746+waellison@users.noreply.github.com>
2022-03-29 15:55:57 +0000

    Started moving styles to component-specific areas.

--- src/App/index.js
+++ src/App/index.js
@@ -3,7 +3,7 @@ import Skills from '../Skills';
 
 function App() {
   return (
-    <div className="App">
+    <div id="resume">
       <Skills />
     </div>
   );
--- src/Icons/index.js
+++ src/Icons/index.js
@@ -1,3 +1,5 @@
+import "./index.css";
+
 const Flask = () => {
     return (
         <span>
