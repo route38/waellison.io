9494b6077ecd0beba8bff3616903578bf0e5523f
William Ellison <waellison@gmail.com>
2022-03-29 20:53:56 -0400

    Added summary to resume view

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,11 @@
 import './index.css';
+import Summary from '../Summary';
 import Skills from '../Skills';
 
 function App() {
   return (
     <div id="resume">
+      <Summary />
       <Skills />
     </div>
   );
