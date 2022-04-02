67a4122132a87cef97b151ec407ff982674ef105
William Ellison <waellison@gmail.com>
2022-04-02 18:21:25 -0400

    Moved resume data into source files.

---
+++ src/resumeData.js
@@ -0,0 +1,93 @@
+const resumeData = {
+    resume: {
+        summary: "I am a passionate, skilled software developer with a strong background in web development and three years' experience in healthcare revenue cycle management.  My skills include programming in Python and JavaScript with the Flask and React frameworks, PostgreSQL databases, and REST APIs.  I am also skilled at developing for the cloud with Docker, Kubernetes, and Microsoft Azure.  I have recognized skills in analysis, problem solving, and written and oral communication.",
+        skills: {
+            "DevOps": [
+                "azure",
+                "googleCloud",
+                "docker",
+                "kubernetes",
+                "git",
+                "linux",
+                "continuousIntegration"
+            ],
+            "Web Development": [
+                "flask",
+                "django",
+                "jQuery",
+                "reactjs",
+                "restApi",
+                "html",
+                "css"
+            ],
+            "Languages": [
+                "python",
+                "javaScript",
+                "cee",
+                "ruby",
+                "postgreSQL",
+                "english",
+                "french"
+            ],
+            "General": [
+                "dependability",
+                "problemSolving",
+                "telecommuting",
+                "writing",
+                "excel",
+                "macOS"
+            ]
+        },
+        experience: [
+            {
+                title: "Accounts Receivable Representative",
+                company: "University Physicians' Association",
+                startDate: "2021-11-15",
+                endDate: null,
+                duties: [
+                    "Averaged 30 claims per day with 98% accuracy.",
+                    "90% remote since March 2020.",
+                    "Trained other representatives on writing appeal letters.",
+                    "Responsible for reducing outstanding claim volume by 50% within one quarter.",
+                    "Recognized as my team's point of knowledge for questions on diagnosis and procedure coding.",
+                    "Submitted more than 300 appeals to all major payers, overturning 70% of denials."
+                ],
+                competencies: [
+                    "Telecommuting",
+                    "Medical coding",
+                    "Medical billing",
+                    "Writing",
+                    "Teamwork",
+                    "Time management",
+                    "Detail orientation",
+                    "Microsoft Excel",
+                    "Microsoft Word",
+                    "Microsoft Access"
+                ]
+            },
+            {
+                title: "Medical Billing Representative",
+                company: "University Physicians' Association",
+                startDate: "2018-09-10",
+                endDate: "2019-11-15",
+                duties: [
+                    "Averaged 1,200 line items per day with 100% accuracy.",
+                    "Worked in a diverse array of medical specialties including surgery, oncology, and public health.",
+                    "Made approved edits in accordance with company and payer guidelines.",
+                    "Contributed to a team responsible for reviewing more than 8,000 line items per day."
+                ],
+                competencies: [
+                    "Medical billing",
+                    "Medical coding",
+                    "Data entry",
+                    "Time management",
+                    "Teamwork",
+                    "Microsoft Excel",
+                    "Detail orientation"
+                ]
+            }
+        ]
+    }
+};
+
+export default resumeData;
