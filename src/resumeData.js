3663cd13c2ad54ac489dae62b34c0020b410fecb
William Ellison <waellison@gmail.com>
2022-04-02 21:38:10 -0400

    Added education, old job

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,92 +1,175 @@
 const resumeData = {
-    resume: {
-        summary: "I am a passionate, skilled software developer with a strong background in web development and three years' experience in healthcare revenue cycle management.  My skills include programming in Python and JavaScript with the Flask and React frameworks, PostgreSQL databases, and REST APIs.  I am also skilled at developing for the cloud with Docker, Kubernetes, and Microsoft Azure.  I have recognized skills in analysis, problem solving, and written and oral communication.",
-        skills: {
-            "DevOps": [
-                "azure",
-                "googleCloud",
-                "docker",
-                "kubernetes",
-                "git",
-                "linux",
-                "continuousIntegration"
-            ],
-            "Web Development": [
-                "flask",
-                "django",
-                "jQuery",
-                "reactjs",
-                "restApi",
-                "html",
-                "css"
-            ],
-            "Languages": [
-                "python",
-                "javaScript",
-                "cee",
-                "ruby",
-                "postgreSQL",
-                "english",
-                "french"
-            ],
-            "General": [
-                "dependability",
-                "problemSolving",
-                "telecommuting",
-                "writing",
-                "excel",
-                "macOS"
+    summary: "I am a passionate, skilled software developer with a strong background in web development and three years' experience in healthcare revenue cycle management.  My skills include programming in Python and JavaScript with the Flask and React frameworks, PostgreSQL databases, and REST APIs.  I am also skilled at developing for the cloud with Docker, Kubernetes, and Microsoft Azure.  I have recognized skills in analysis, problem solving, and written and oral communication.",
+    skills: {
+        "DevOps": [
+            "azure",
+            "googleCloud",
+            "docker",
+            "kubernetes",
+            "git",
+            "linux",
+            "continuousIntegration"
+        ],
+        "Web Development": [
+            "flask",
+            "django",
+            "jQuery",
+            "reactjs",
+            "restApi",
+            "html",
+            "css"
+        ],
+        "Languages": [
+            "python",
+            "javaScript",
+            "cee",
+            "ruby",
+            "postgreSQL",
+            "english",
+            "french"
+        ],
+        "General": [
+            "dependability",
+            "problemSolving",
+            "telecommuting",
+            "writing",
+            "excel",
+            "macOS"
+        ]
+    },
+    education: {
+        fcc: {
+            institution: "freeCodeCamp",
+            credential: "Certification in Frontend Web Libraries (in-progress)",
+            startDate: "2022-03-15",
+            endDate: null,
+            description: "Self-paced boot camp-style curriculum in major libraries used for frontend Web development, including jQuery, React, Redux, and Bootstrap.",
+            competencies: [
+                "JavaScript",
+                "React.js",
+                "Redux",
+                "HTML",
+                "CSS",
+                "Bootstrap"
             ]
         },
-        experience: [
-            {
-                title: "Accounts Receivable Representative",
-                company: "University Physicians' Association",
-                startDate: "2021-11-15",
-                endDate: null,
-                duties: [
-                    "Averaged 30 claims per day with 98% accuracy.",
-                    "90% remote since March 2020.",
-                    "Trained other representatives on writing appeal letters.",
-                    "Responsible for reducing outstanding claim volume by 50% within one quarter.",
-                    "Recognized as my team's point of knowledge for questions on diagnosis and procedure coding.",
-                    "Submitted more than 300 appeals to all major payers, overturning 70% of denials."
-                ],
-                competencies: [
-                    "Telecommuting",
-                    "Medical coding",
-                    "Medical billing",
-                    "Writing",
-                    "Teamwork",
-                    "Time management",
-                    "Detail orientation",
-                    "Microsoft Excel",
-                    "Microsoft Word",
-                    "Microsoft Access"
-                ]
-            },
-            {
-                title: "Medical Billing Representative",
-                company: "University Physicians' Association",
-                startDate: "2018-09-10",
-                endDate: "2019-11-15",
-                duties: [
-                    "Averaged 1,200 line items per day with 100% accuracy.",
-                    "Worked in a diverse array of medical specialties including surgery, oncology, and public health.",
-                    "Made approved edits in accordance with company and payer guidelines.",
-                    "Contributed to a team responsible for reviewing more than 8,000 line items per day."
-                ],
-                competencies: [
-                    "Medical billing",
-                    "Medical coding",
-                    "Data entry",
-                    "Time management",
-                    "Teamwork",
-                    "Microsoft Excel",
-                    "Detail orientation"
-                ]
-            }
-        ]
+        nucamp: {
+            institution: "Nucamp",
+            credential: "Certification in Backend Development with Python, SQL, and DevOps",
+            startDate: "2021-08-03",
+            endDate: "2021-11-20",
+            description: "Boot camp course in modern backend Web development with the Python language, PostgreSQL, Flask, Django, and DevOps deployments to Microsoft Azure, Google Cloud, and Amazon AWS.",
+            competencies: [
+                "Python",
+                "Flask",
+                "PostgreSQL",
+                "Microsoft Azure",
+                "Git",
+                "CI/CD",
+                "Visual Studio Code"
+            ]
+        },
+        tcatk: {
+            institution: "Tennessee College of Applied Technology",
+            credential: "Diploma in Medical Office Information Technology",
+            startDate: "2018-05-01",
+            endDate: "2018-08-24",
+            description: "Broad-ranging instruction in concepts and procedures of the medical billing office, including reimbursement principles, medical terms, medical coding, and practice-management software.",
+            competencies: [
+                "Medical coding",
+                "Medical billing",
+                "Microsoft Word",
+                "Microsoft Excel",
+                "Medical terminology",
+                "Teamwork"
+            ]
+        },
+        utk: {
+            institution: "University of Tennessee",
+            credential: "Bachelor of Science in Computer Science",
+            startDate: "2006-08-20",
+            endDate: "2011-05-13",
+            description: "Practical and theoretical instruction in computer science, including systems programming, object-oriented systems, graphical user interfaces, technical writing, and theoretical computer science, combined with traditional liberal-arts curriculum.",
+            competencies: [
+                "C++",
+                "Java",
+                "Technical writing",
+                "Teamwork",
+                "Public speaking",
+                "LaTeX",
+                "OpenGL",
+                "Subversion",
+                "Emacs"
+            ]
+        }
+    },
+    experience: {
+        upaAR: {
+            title: "Accounts Receivable Representative",
+            company: "University Physicians' Association",
+            startDate: "2019-11-18",
+            endDate: null,
+            duties: [
+                "Averaged 30 claims per day with 98% accuracy.",
+                "90% remote since March 2020.",
+                "Trained other representatives on writing appeal letters.",
+                "Responsible for reducing outstanding claim volume by 50% within one quarter.",
+                "Recognized as my team's point of knowledge for questions on diagnosis and procedure coding.",
+                "Submitted more than 300 appeals to all major payers, overturning 70% of denials."
+            ],
+            competencies: [
+                "Telecommuting",
+                "Medical coding",
+                "Medical billing",
+                "Writing",
+                "Teamwork",
+                "Time management",
+                "Detail orientation",
+                "Microsoft Excel",
+                "Microsoft Word",
+                "Microsoft Access"
+            ]
+        },
+        upaBilling: {
+            title: "Medical Billing Representative",
+            company: "University Physicians' Association",
+            startDate: "2018-09-10",
+            endDate: "2019-11-15",
+            duties: [
+                "Averaged 1,200 line items per day with 100% accuracy.",
+                "Worked in a diverse array of medical specialties including surgery, oncology, and public health.",
+                "Made approved edits in accordance with company and payer guidelines.",
+                "Contributed to a team responsible for reviewing more than 8,000 line items per day."
+            ],
+            competencies: [
+                "Medical billing",
+                "Medical coding",
+                "Data entry",
+                "Time management",
+                "Teamwork",
+                "Microsoft Excel",
+                "Detail orientation"
+            ]
+        },
+        wci: {
+            title: "Office Cleaner",
+            company: "We Clean It CJS",
+            startDate: "2006-08-01",
+            endDate: "2018-09-09",
+            duties: [
+                "Cleaned a total of 50,000 sq ft of office space per night.",
+                "Achieved a complaint rate of 0% for 7 consecutive years.",
+                "Created the company website using WordPress and the <abbr title='Linux, nginx, MariaDB, and PHP'>LEMP</abbr> stack."
+            ],
+            competencies: [
+                "Time management",
+                "Teamwork",
+                "Detail orientation",
+                "WordPress",
+                "Linux",
+                "nginx"
+            ]
+        }
     }
 };
 
