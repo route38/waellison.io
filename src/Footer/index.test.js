363bd683c40db6a53eef25be6b7049e1e40df1f0
William Ellison <waellison@gmail.com>
2022-04-18 20:22:54 -0400

    Added test for Footer component

---
+++ src/Footer/index.test.js
@@ -0,0 +1,8 @@
+import renderer from "react-test-renderer";
+import Footer from './index';
+
+it('renders correct footer component', () => {
+    const component = renderer.create(<Footer />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
