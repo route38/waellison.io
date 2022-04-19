c462da36390ae769df4778245ed64a04fe187e83
William Ellison <waellison@gmail.com>
2022-04-18 20:18:12 -0400

    Added a test for the summary component.

---
+++ src/Resume/Summary/index.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Summary from './index';
+
+it('renders correct summary component', () => {
+    const component = renderer.create(<Summary />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
