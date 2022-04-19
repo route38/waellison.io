10f4f658262577f26b27404c3ae2ed13bda7e74c
William Ellison <waellison@gmail.com>
2022-04-18 20:16:51 -0400

    Fixed a typo in the school component test

--- src/Resume/EducationBlock/School/index.test.js
+++ src/Resume/EducationBlock/School/index.test.js
@@ -3,7 +3,7 @@ import renderer from "react-test-renderer";
 import School from './index';
 import resumeData from '../../../resumeData';
 
-it('renders correct footer component', () => {
+it('renders correct school component', () => {
     const aSchool = resumeData.education.fcc;
     const component = renderer.create(<School {...aSchool} />);
     const tree = component.toJSON();
