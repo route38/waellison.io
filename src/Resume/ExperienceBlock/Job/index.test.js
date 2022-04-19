09f2d79f8984be1f1b048ddd0c9b559578edb810
William Ellison <waellison@gmail.com>
2022-04-18 20:15:13 -0400

    Added tests for the job component

---
+++ src/Resume/ExperienceBlock/Job/index.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Job from './index';
+import resumeData from '../../../resumeData';
+
+it('renders correct job component', () => {
+    const aJob = resumeData.experience.upaAR;
+    const component = renderer.create(<Job {...aJob} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
