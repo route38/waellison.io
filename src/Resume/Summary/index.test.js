ac9c49ccb3053d5a7a3dc523fba513109f789e64
William Ellison <waellison@gmail.com>
2022-04-18 20:18:50 -0400

    Updated summary test to reflect text prop.

--- src/Resume/Summary/index.test.js
+++ src/Resume/Summary/index.test.js
@@ -3,7 +3,7 @@ import renderer from "react-test-renderer";
 import Summary from './index';
 
 it('renders correct summary component', () => {
-    const component = renderer.create(<Summary />);
+    const component = renderer.create(<Summary text="This is the summary." />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
