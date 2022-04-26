00107e9cf4b3650bddb74adfdecf826e43e747f3
William Ellison <waellison@gmail.com>
2022-04-26 11:48:57 -0400

    Commited renames of old JS files

--- src/Resume/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Resume from "./index";
-
-it("renders correct resume component", () => {
-  const resumeRender = renderer.create(<Resume />);
-  const tree = resumeRender.toJSON();
-  expect(tree).toMatchSnapshot();
-});
