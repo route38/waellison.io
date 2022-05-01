d0115f802e2e5884be87446031dd33b8179a9022
William Ellison <97247746+waellison@users.noreply.github.com>
2022-05-01 15:48:13 -0400

    Adding React Router back in.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import Resume from "./Resume";
+import Portfolio from "./Portfolio";
 import App from "./App";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 import "./index.css";
 
 const docRoot: HTMLElement | null = document.getElementById("root");
 const root = createRoot(docRoot!);
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <NavBar/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="/resume" element={<Resume/>}/>
+                <Route path="/portfolio" element={<Portfolio/>}/>
+            </Routes>
+        </BrowserRouter>
+        <Footer/>
+    </React.StrictMode>
+);
