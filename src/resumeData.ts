58a49d02874630c4f8eab859d4c6bf0f2d38d14c
William Ellison <waellison@gmail.com>
2022-05-07 13:00:38 -0400

    update: rewrote resume data describing jobs

--- src/resumeData.ts
+++ src/resumeData.ts
@@ -37,25 +37,18 @@ export type JobType = {
 
 export const resumeData: ResumeData = {
   summary:
-    "I am a passionate, skilled software developer with a strong background in web development and three years' experience in healthcare revenue cycle management.  My skills include programming in Python and JavaScript with the Flask and React frameworks, PostgreSQL databases, and REST APIs.  I am also skilled at developing for the cloud with Docker, Kubernetes, and Microsoft Azure.  I have recognized skills in analysis, problem solving, and written and oral communication.",
+    "I am a passionate, skilled software developer with a strong background in web development and three years' experience in healthcare revenue cycle management.  My skills include programming in Python and JavaScript with the Flask and React frameworks, PostgreSQL databases, and REST APIs.  I am also skilled at developing for the cloud with Docker, Kubernetes, and Microsoft Azure.  I have recognized skills in analysis, problem solving, training and coaching, and written and oral communication.",
   skills: {
-    DevOps: [
-      "azure",
-      "docker",
-      "kubernetes",
-      "git",
-      "linux",
-      "continuousIntegration",
-    ],
+    DevOps: ["azure", "docker", "git", "linux", "continuousIntegration"],
     Libraries: ["flask", "jQuery", "reactjs", "restApi", "postgreSQL"],
     Languages: ["python", "javaScript", "ceePlusPlus", "english", "french"],
   },
   education: {
     fcc: {
       institution: "freeCodeCamp",
-      credential: "Certification in Frontend Web Libraries (in-progress)",
+      credential: "Certifications in Responsive Web Design and JavaScript Algorithms and Data Structures",
       startDate: "2022-03-15",
-      endDate: undefined,
+      endDate: "2022-04-30",
       description:
         "Self-paced boot camp-style curriculum in major libraries used for frontend Web development, including jQuery, React, Redux, and Bootstrap.",
       competencies: [
@@ -67,6 +60,20 @@ export const resumeData: ResumeData = {
         "Bootstrap",
       ],
     },
+    algoexpert: {
+        institution: "AlgoExpert",
+        credential: "Certificate of Completion",
+        startDate: "2022-01-14",
+        endDate: "2022-04-22",
+        description:
+          "Self-paced curriculum in algorithms and data structures, including linked lists, arrays, graphs, trees, strings, recursion, and dynamic programming.",
+        competencies: [
+          "Python",
+          "TypeScript",
+          "Algorithms",
+          "Data structures"
+        ]
+    },
     nucamp: {
       institution: "Nucamp",
       credential:
@@ -74,7 +81,7 @@ export const resumeData: ResumeData = {
       startDate: "2021-08-03",
       endDate: "2021-11-20",
       description:
-        "Boot camp course in modern backend Web development with the Python language, PostgreSQL, Flask, Django, and DevOps deployments to Microsoft Azure, Google Cloud, and Amazon AWS.",
+        "<p>Boot camp course in modern backend Web development with the Python language, PostgreSQL, Flask, Django, and DevOps deployments to Microsoft Azure, Google Cloud, and Amazon AWS.</p><p>Offered coaching and assistance to classmates throughout the course.</p>",
       competencies: [
         "Python",
         "Flask",
@@ -91,7 +98,7 @@ export const resumeData: ResumeData = {
       startDate: "2018-05-01",
       endDate: "2018-08-24",
       description:
-        "Broad-ranging instruction in concepts and procedures of the medical billing office, including reimbursement principles, medical terms, medical coding, and practice-management software.",
+        "<p>Broad-ranging instruction in concepts and procedures of the medical billing office, including reimbursement principles, medical terms, medical coding, and practice-management software.</p><p>Offered coaching and assistance to classmates throughout the course.</p>",
       competencies: [
         "Medical coding",
         "Medical billing",
@@ -128,12 +135,12 @@ export const resumeData: ResumeData = {
       startDate: "2019-11-18",
       endDate: undefined,
       duties: [
-        "Averaged 30 claims per day with 98% accuracy.",
-        "90% remote since March 2020.",
-        "Trained other representatives on writing appeal letters.",
-        "Responsible for reducing outstanding claim volume by 50% within one quarter.",
-        "Recognized as my team's point of knowledge for questions on diagnosis and procedure coding.",
-        "Submitted more than 300 appeals to all major payers, overturning 70% of denials.",
+        "Collaborated with a 10-member cross-functional team that reduced outstanding claims by 40% quarter-over-quarter, reducing A/R days by 33%",
+        "Evaluated and resolved an average of 35 claims per day with 98% accuracy",
+        "Assessed denied claims and reviewed for appeal processes",
+        "Drafted appeal letters to all major carriers, overturning 70% of denials",
+        "Recognized as my team's point of knowledge for questions on diagnosis and procedure coding",
+        "Guided and trained personnel within a 55-member department, training on coding rules, standard workflows, software usage, and appeal letter origination",
       ],
       competencies: [
         "Telecommuting",
@@ -154,10 +161,11 @@ export const resumeData: ResumeData = {
       startDate: "2018-09-10",
       endDate: "2019-11-15",
       duties: [
-        "Averaged 1,200 line items per day with 100% accuracy.",
-        "Worked in a diverse array of medical specialties including surgery, oncology, and public health.",
-        "Made approved edits in accordance with company and payer guidelines.",
-        "Contributed to a team responsible for reviewing more than 8,000 line items per day.",
+        "Partnered with an established 10-member team to review more than 8,000 outbound line items per day",
+        "Reviewed 1,200 line items per day with 100% accuracy",
+        "Increased clean claim rate by 5% on average by practice, reducing A/R overhead and turnaround time",
+        "Analyzed an average of $100,000 of charges per day for inaccuracies",
+        "Proposed workflow alterations which led to 10% faster turnaround of paper charge batches"
       ],
       competencies: [
         "Medical billing",
@@ -175,9 +183,10 @@ export const resumeData: ResumeData = {
       startDate: "2006-08-01",
       endDate: "2018-09-09",
       duties: [
-        "Cleaned a total of 50,000 sq ft of office space per night.",
-        "Achieved a complaint rate of 0% for 7 consecutive years.",
-        "Created the company website using WordPress and the <abbr title='Linux, nginx, MariaDB, and PHP'>LEMP</abbr> stack.",
+        "Supervised a two-person cleaning crew maintaining 50,000 square feet per night",
+        "Led review initiatives of cleaning and training protocols, increasing productivity by 33%",
+        "Redeveloped internal tracking systems to reduce lead time on bids by 50%",
+        "Implemented a company website that doubled inbound sales calls",
       ],
       competencies: [
         "Time management",
