c77aecfb4bcf8e4a8b75684f7177ae6c24ec4e19
William Ellison <waellison@gmail.com>
2022-05-12 22:16:07 -0400

    enhancement: Improved front page with an animation.

--- src/FrontPage/index.tsx
+++ src/FrontPage/index.tsx
@@ -5,15 +5,17 @@ class FrontPage extends React.Component {
     render() {
         return (
             <div className="container" id="frontPage">
-                <h1>Hi.  I'm William.</h1>
-                <p>
-                    I make fast, dumb machines do slow, smart things as quickly as possible.
-                </p>
-                <p>
-                    <Link to="/resume" className="ctaButton">
-                        See what I can do.
-                    </Link>
-                </p>
+                <div className="jumbotron">
+                    <h1>Hi.  I'm William.</h1>
+                    <p>
+                        I make fast, dumb machines do slow, smart things as quickly as possible.
+                    </p>
+                    <p>
+                        <Link to="/resume" className="ctaButton">
+                            See what I can do.
+                        </Link>
+                    </p>
+                </div>
             </div>
         );
     }
